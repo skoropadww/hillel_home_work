e9f58622ad68843cc902af3dcca8948d2887de4e
Vitalii <skoropadww@gmail.com>
2022-04-05 14:46:29 +0300

    home work 9 lessons

--- home_work9_lessons/it-school.js
+++ home_work9_lessons/it-school.js
@@ -1,22 +1,60 @@
 const itSchool = {
-    // свойства
+    name: "Simple online school",
+    description: "Simple online school description",
+    maxGroupCount: 5,
+    maxStudentsCountPerGroup: 13,
+    availableCourses: ["Front-end Pro", "Front-end Basic"],
+    startedGroups: [],
+    __callback: {},
 
-    // свойства-массивы
+    __supportedEventTypes: { 
+      GROUP_STARTED: "GROUP_STARTED", 
+      GROUP_ENDED: "GROUP_ENDED"},
 
-    // методы 
+  startLearningGroup(courseName, amountOfStudents){
+    if (this.availableCourses.includes(courseName)){
+      if (amountOfStudents <= this.maxStudentsCountPerGroup){
+        if (!this.startedGroups.some((startedGroup) => startedGroup.courseName === courseName)){
+          this.startedGroups.push({ courseName, amountOfStudents})
+          this.dispatch(this.__supportedEventTypes.GROUP_STARTED, courseName)
+        }else {
+          console.log(`Started ${courseName} group!`);
+        }
+      } else {
+        console.log(`We not supported ${amountOfStudents} amount of students`);
+      }
+    } else {
+      console.log(`Sorry, course ${courseName} not supported yet.`);
+    }
+  },
 
+  endLearningGroup(courseName){
+    if (this.startedGroups.some((startedGroup) => startedGroup.courseName === courseName)){
+      this.startedGroups = this.startedGroups.filter((startedGroup) => startedGroup.courseName !== courseName);
+      this.dispatch(this.__supportedEventTypes.GROUP_ENDED, courseName);
+    } else {
+      console.log(`You are trying to finish not existing learning group`);
+    }
+  },
 
+  on(eventName, callback){
+    if (eventName in this.__supportedEventTypes) this.__callback[eventName] = callback;
+  },
+
+  dispatch(eventName,data){
+    this.__callback[eventName] && this.__callback[eventName](data);
+  }
 };
 
 itSchool.on(
-    "GROUP_STARTED",
-    (courseName) => console.log(`О, стартовала новая группа по курсу ${courseName}!`),
+  itSchool.__supportedEventTypes.GROUP_STARTED,
+  (courseName) => console.log(`О, стартовала новая группа по курсу ${courseName}!`),
 );
 
 
 itSchool.on(
-    "GROUP_ENDED",
-    (courseName) => console.log(`О, похоже группа по курсу ${courseName} закончила свое обучение!`),
+  itSchool.__supportedEventTypes.GROUP_ENDED,
+  (courseName) => console.log(`Group whith ${courseName} course successfully finished`),
 );
 
 
